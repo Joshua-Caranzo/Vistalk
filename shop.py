f5a4720eefdb6c27b1253b8823d0d08eb577a244
Joshua-Caranzo <93688009+Joshua-Caranzo@users.noreply.github.com>
2024-11-24 23:18:02 +0800

    Update shop.py

--- shop.py
+++ shop.py
@@ -255,7 +255,7 @@ def get_items():
     elif itemTypeID == '3':
         count_query = """
             SELECT COUNT(*) as total 
-            FROM coinBag cb 
+            FROM coinbag cb 
             WHERE cb.isActive = true
         """
         if searchString:
